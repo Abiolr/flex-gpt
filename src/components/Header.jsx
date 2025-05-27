07c2abdcd01c4e9f5a4d1f54cb73d862432f6158
Abiolr <abiolaraji5@outlook.com>
2025-05-27 17:21:18 -0600

    initializing repo

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header () {
+    return (
+        <div>
+            <img className="logo" src="src/assets/logo.png" alt="logo"/>
+            <h1>FlexGPT</h1>
+        </div>
+    )
+}
