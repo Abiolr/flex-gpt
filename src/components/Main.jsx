91f7712d9935060030f156a65a596c9d6f9ff903
Abiolr <abiolaraji5@outlook.com>
2025-05-28 15:54:53 -0600

    saving changes for now

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,19 +4,30 @@ export default function Main () {
     const [equipmentItems, setEquipmentItems] = useState([])
     
     const listEquipmentItems = equipmentItems.map((equipmentItem) =>
-        <li key={equipmentItem}>{equipmentItem}</li>)
+        <li key={equipmentItem}>{equipmentItem}
+            <button
+                aria-label="delete"
+            >
+                ⨉
+            </button>
+        </li>)
 
     function submitForm(event) {
         event.preventDefault()
-        const formData = new FormData(event.currentTarget)
+        const formElement = event.currentTarget
+        const formData = new FormData(formElement)
         const newEquipmentItem = formData.get("equipmentItem").trim()
-        if (newEquipmentItem != "")
-            setEquipmentItems(prevItems => [...prevItems, newEquipmentItem])
+        if (newEquipmentItem != ""){
+            setEquipmentItems(prevItems =>
+                [...prevItems, newEquipmentItem])
+        }
+        formElement.reset()
     }
 
     return (
         <main>
-            <form onSubmit={submitForm} className="equipment-form">
+            <form onSubmit={submitForm}
+                className="equipment-form">
                 <input
                     type="text"
                     placeholder="e.g. dumbbells"
@@ -24,7 +35,19 @@ export default function Main () {
                     name="equipmentItem"/>
                 <button>Add equipment</button>
             </form>
+            {equipmentItems.length > 0 && (
+            <>
+                <h1 className="renderHeader">Equipment on hand:</h1>
             <ul>{listEquipmentItems}</ul>
+            <div className="genWorkout">
+                <div>
+                    <h1>Ready for a workout?</h1>
+                    <p>Generate a full-body workout based on your list of equipment </p>
+                </div>
+                <button>Generate workout</button>
+            </div>
+            </>
+            )}
         </main>
     )
 }
