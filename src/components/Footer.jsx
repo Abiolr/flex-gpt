b572759824b8351c6fa46cc5de77e0e0b5895172
Abiolr <abiolaraji5@outlook.com>
2025-05-29 16:53:28 -0600

    fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,21 @@
-export default function Footer () {
+import GitHubLink from "http://github.com/Abiolr"
+import LinkedinLink from "http://linkedin.com/in/abiola-raji/"
+
+export default function Footer() {
     return (
         <footer>
             <small>
                 © Abiola Raji 2025
                 <div>
-                <a>GitHub</a>
-                <> | </> 
-                <a>Linkedin</a>
+                    <a href={GitHubLink} target="_blank" rel="noopener noreferrer">
+                        GitHub
+                    </a>
+                    <span> | </span>
+                    <a href={LinkedinLink} target="_blank" rel="noopener noreferrer">
+                        LinkedIn
+                    </a>
                 </div>
             </small>
         </footer>
-   )
+    )
 }
